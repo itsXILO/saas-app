7cc7e713099e95b3fb9e835a6b6359cd733802d9
itsXILO <scienceunveiled1@gmail.com>
2026-07-04 23:11:44 +0530

    remove-obj

--- src/pages/RemoveBg.jsx
+++ src/pages/RemoveBg.jsx
@@ -19,6 +19,7 @@ const RemoveBg = () => {
             accept="image/*"
             className='w-full mt-2 text-sm text-slate-300 file:mr-4 file:py-2 file:px-4 file:rounded-md file:border-0 file:text-sm file:font-medium file:bg-slate-700 file:text-slate-200 hover:file:bg-slate-600 cursor-pointer'
           />
+          <p className='mt-2 text-xs text-gray-500'>Supported formats: PNG, JPEG, WEBP</p>
           <button className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#226BFF] to-[#65ADFF] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer'>
             <Eraser className='w-5'/>
             Remove Background
