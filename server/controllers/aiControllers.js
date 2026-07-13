a5bf486a8b80f4e79cde55a09490356d05723b83
itsXILO <scienceunveiled1@gmail.com>
2026-07-13 21:09:34 +0530

    clerk-auth

---
+++ server/controllers/aiControllers.js
@@ -0,0 +1,23 @@
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+    apiKey: process.env.GEMINI_API_KEY,
+    baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/"
+});
+
+
+export const generateArticle = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const { prompt, length } = req.body;
+    const plan = req.plan;
+    const free_usage = req.free_usage;
+
+    if(plan !== 'premium' && free_usage >= 10){
+    return res.json({ success: false, message: "Limit reached. Upgrade to continue."})
+}
+
+
+  } catch (error) {
+  }
+}
